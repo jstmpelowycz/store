167da9f24dc32d3de0876dcd4bf54a7d5e39e9df
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-04-27 16:42:41 +0300

    constraint only_two_stored_products added

--- api/db/createQueries/storeProduct.ts
+++ api/db/createQueries/storeProduct.ts
@@ -3,20 +3,21 @@ import {TABLES} from "../tables";
 export const CREATE_STORE_PRODUCTS_TABLE = `
     CREATE TABLE ${TABLES.storeProduct}
     (
-        upc             VARCHAR(12)   NOT NULL PRIMARY KEY,
-        upc_prom        VARCHAR(12) UNIQUE,
-        selling_price   NUMERIC(13,4) NOT NULL
+        upc            VARCHAR(12)    NOT NULL PRIMARY KEY,
+        upc_prom       VARCHAR(12) UNIQUE,
+        selling_price  NUMERIC(13, 4) NOT NULL
             CHECK (selling_price > 0),
-        amount          INT           NOT NULL 
+        amount         INT            NOT NULL
             CHECK (amount > 0),
-        is_promotional  BOOLEAN       NOT NULL,
-        product_id      INT           NOT NULL UNIQUE,
-        FOREIGN KEY (product_id)
+        is_promotional BOOLEAN        NOT NULL,
+        product_id     INT            NOT NULL UNIQUE,
+        CONSTRAINT only_two_stored_products UNIQUE (product_id, is_promotional)
+            FOREIGN KEY (product_id)
             REFERENCES ${TABLES.products}(id)
             ON UPDATE CASCADE
             ON DELETE NO ACTION,
-        FOREIGN KEY (upc_prom) 
-            REFERENCES ${TABLES.storeProduct}(upc)
+        FOREIGN KEY (upc_prom)
+            REFERENCES ${TABLES.storeProduct} (upc)
             ON UPDATE CASCADE
             ON DELETE SET NULL
     )
