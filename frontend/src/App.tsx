1c3c5364950aa60acb5499e82ec4d481dad89eee
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-04-29 16:00:18 +0300

    code refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {Login} from "./components/Login/Login.tsx";
 // @ts-ignore
 import {SignUp} from "./components/SignUp/SignUp.tsx";
 // @ts-ignore
-import {Toolbar} from "./components/MainPage/Toolbar.tsx";
+import {Toolbar} from "./components/Toolbar/Toolbar.tsx";
 
 export const App = () => {
   return <Toolbar/>
--- frontend/src/components/Toolbar/Toolbar.tsx
+++ frontend/src/components/Toolbar/Toolbar.tsx
@@ -4,11 +4,11 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export const Toolbar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" role="navigation">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand href="#">My Toolbar</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" role="navigation">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
