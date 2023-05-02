e6284b26506d402c3dca26981b28f9a4e05ef71c
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-05-02 14:21:16 +0300

    React API is deprecated fixed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,20 @@
 // @ts-ignore
 import React, {StrictMode} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {render} from 'react-dom';
 // @ts-ignore
 import {App} from './App.tsx';
 // @ts-ignore
 import {AppContextProvider} from "./context/AppContext.tsx";
+import {createRoot} from 'react-dom/client';
 
 const withStrictModeAndContext = () => (
-  <StrictMode>
-    <AppContextProvider>
-      <App/>
-    </AppContextProvider>
-  </StrictMode>
+    <StrictMode>
+        <AppContextProvider>
+            <App/>
+        </AppContextProvider>
+    </StrictMode>
 );
 
-const root = document.getElementById('root');
-
-render(withStrictModeAndContext(), root);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(withStrictModeAndContext());
