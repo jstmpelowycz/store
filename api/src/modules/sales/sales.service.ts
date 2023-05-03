14e67668125cb626ea5a6b108a1d0dbdc6a4d775
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-05-03 07:57:35 +0300

    selling api is improved

--- api/src/modules/sales/sales.service.ts
+++ api/src/modules/sales/sales.service.ts
@@ -7,59 +7,93 @@ import {SalesError} from "./sales.constants";
 type CreateSaleWithPromotionFields = Omit<CreateSaleFields, 'selling_price'>;
 
 export class SalesService {
-  private salesRepository = new SalesRepository();
+    private salesRepository = new SalesRepository();
 
-  public async createAndApplyPromotion(
-    fields: CreateSaleWithPromotionFields,
-  ): Promise<Sale> {
-    const {store_product_upc, invoice_id, amount} = fields;
+    public async createAndApplyPromotion(
+        fields: CreateSaleWithPromotionFields,
+    ): Promise<Sale> {
+        const {store_product_upc, invoice_id, amount} = fields;
 
-    const {rows} = await pool.query({
-      text: `
-          SELECT CASE
-                     WHEN sp.is_promotional
-                         THEN ($1 * sp.selling_price * 0.8)
-                     ELSE ($1 * sp.selling_price)
-                     END
-          FROM store_products sp
-          WHERE sp.upc = $2;
-      `,
-      values: [amount, store_product_upc],
-    });
+        const {rows} = await pool.query({
+            text: `
+                SELECT CASE
+                           WHEN sp.is_promotional
+                               THEN ($1 * sp.selling_price * 0.8)
+                           ELSE ($1 * sp.selling_price)
+                           END
+                FROM store_products sp
+                WHERE sp.upc = $2;
+            `,
+            values: [amount, store_product_upc],
+        });
 
-    const sellingPrice = rows[0];
+        const sellingPrice = rows[0];
 
-    await this.verifySellingAmount({
-      store_product_upc,
-      amount,
-    });
+        await this.verifySellingAmount({
+            store_product_upc,
+            amount,
+        });
 
-    return this.salesRepository.create({
-      store_product_upc,
-      invoice_id,
-      amount,
-      selling_price: sellingPrice,
-    });
-  }
+        await this.updateStoredAmount({
+          store_product_upc,
+          amount,
+        })
 
-  private async verifySellingAmount(
-    options: Omit<CreateSaleWithPromotionFields, 'invoice_id'>,
-  ): Promise<Throwable<void>> {
-    const {store_product_upc, amount} = options;
-
-    const {rows} = await pool.query({
-      text: `
-          SELECT amount
-          FROM store_products
-          WHERE upc = $1
-      `,
-      values: [store_product_upc]
-    });
-
-    const retrievedAmount = rows[0];
-
-    if (retrievedAmount < amount) {
-      throw new Error(SalesError.RequestedAmountNotAvailable);
+        return this.salesRepository.create({
+            store_product_upc,
+            invoice_id,
+            amount,
+            selling_price: sellingPrice,
+        });
     }
-  }
+
+    private async updateStoredAmount(
+        options: Omit<CreateSaleWithPromotionFields, 'invoice_id'>,
+    ): Promise<Throwable<void>> {
+        const {store_product_upc, amount} = options;
+
+        const {rows} = await pool.query({
+            text: `
+                SELECT amount
+                FROM store_products
+                WHERE upc = $1
+            `,
+            values: [store_product_upc]
+        });
+
+        const retrievedAmount = rows[0];
+
+        const updatedAmount = retrievedAmount - amount;
+
+        await pool.query({
+            text: `
+                UPDATE store_products
+                SET amount = $2
+                WHERE upc = $1
+            `,
+            values: [store_product_upc, updatedAmount]
+        })
+
+    }
+
+    private async verifySellingAmount(
+        options: Omit<CreateSaleWithPromotionFields, 'invoice_id'>,
+    ): Promise<Throwable<void>> {
+        const {store_product_upc, amount} = options;
+
+        const {rows} = await pool.query({
+            text: `
+                SELECT amount
+                FROM store_products
+                WHERE upc = $1
+            `,
+            values: [store_product_upc]
+        });
+
+        const retrievedAmount = rows[0];
+
+        if (retrievedAmount < amount) {
+            throw new Error(SalesError.RequestedAmountNotAvailable);
+        }
+    }
 }
